ebbf25a3b2b83e38ee54a2fda6c5b089ed271284
KimIcexy <kimicexy2002@gmail.com>
2023-08-17 15:15:35 +0700

    focused navigation

--- frontend/ExpoApp/App.js
+++ frontend/ExpoApp/App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { Text, View, ImageBackground } from 'react-native';
-import { Entypo, FontAwesome, Feather, Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Gift from './Screens/Gift';
@@ -16,11 +15,18 @@ import Post from './Screens/Post';
 import Profile from './Screens/Profile';
 import Task from './Screens/Task';
 import Vote from './Screens/Vote';
-const Tab = createBottomTabNavigator();
 import * as CONST from './Utils/constants';
 import styles from './Utils/styles';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { Iconify } from 'react-native-iconify';
+
+const Tab = createBottomTabNavigator();
+const screenOpts = {
+  headerShown: false,
+  tabBarActiveTintColor: CONST.NAVIGATION_ACTIVE_COLOR,
+  height: 62,
+}
 
 export default function App() {
 
@@ -45,31 +51,60 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
+
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{
-        tabBarActiveTintColor: CONST.NAVIGATION_COLOR
-      }}>
-        <Tab.Screen name="Home" component={Home} options={{
-          headerShown: false,
-          tabBarIcon: () => <Entypo name="home" size={24} color="black" />
-        }} />
-        <Tab.Screen name="Map" component={Map} options={{
-          headerShown: false,
-          tabBarIcon: () => <Feather name="map-pin" size={24} color="black" />
-        }} />
-        <Tab.Screen name="Join" component={Join} options={{
-          headerShown: false,
-          tabBarIcon: () => <Ionicons name="add-circle-outline" size={24} color="black" />
-        }} />
-        <Tab.Screen name="Notifications" component={Noti} options={{
-          headerShown: false,
-          tabBarIcon: () => <Ionicons name="notifications-outline" size={24} color="black" />
-        }} />
-        <Tab.Screen name="Profile" component={Profile} options={{
-          headerShown: false,
-          tabBarIcon: () => <Ionicons name="person" size={24} color="black" />
-        }} />
+      <Tab.Navigator screenOptions={screenOpts}>
+        <Tab.Screen name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              if (focused)
+                return <Iconify icon="iconamoon:home-fill" size={24} color={CONST.NAVIGATION_ACTIVE_COLOR} />
+              else
+                return <Iconify icon="iconamoon:home-light" size={24} color="black" />
+            }
+          }} />
+        <Tab.Screen name="Map"
+          component={Map}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              if (focused)
+                return <Iconify icon="bxs:map" size={24} color={CONST.NAVIGATION_ACTIVE_COLOR} />
+              else
+                return <Iconify icon="bx:map" size={24} color="black" />
+            }
+          }} />
+        <Tab.Screen name="Join"
+          component={Join}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              if (focused)
+                return <Iconify icon="subway:add" size={24} color={CONST.NAVIGATION_ACTIVE_COLOR} />
+              else
+                return <Iconify icon="gg:add" size={24} color="black" />
+            }
+          }} />
+        <Tab.Screen name="Notifications"
+          component={Noti}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              if (focused)
+                return <Iconify icon="mingcute:notification-fill" size={24} color={CONST.NAVIGATION_ACTIVE_COLOR} />
+              else
+                return <Iconify icon="mingcute:notification-line" size={24} color="black" />
+            }
+          }} />
+        <Tab.Screen name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              if (focused)
+                return <Iconify icon="iconamoon:profile-fill" size={24} color={CONST.NAVIGATION_ACTIVE_COLOR} />
+              else
+                return <Iconify icon="iconamoon:profile-light" size={24} color="black" />
+            }
+          }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- frontend/ExpoApp/Utils/constants.js
+++ frontend/ExpoApp/Utils/constants.js
@@ -1,22 +1,16 @@
-import { Dimensions, PixelRatio } from 'react-native';
+import { Dimensions} from 'react-native';
 
 // Color
 export const HEADING2_COLOR = "#0A7674";
-export const NAVIGATION_COLOR = "#28A2B2";
+export const NAVIGATION_ACTIVE_COLOR = "#28A2B2";
 export const BACKGROUND_COLOR = "#FFF";
 export const FEATURE_COLOR = '#78D9D8';
 export const FEATURE_TEXT_COLOR = '#206F79';
 
 // Responsive size
 export const STANDARD_SCREEN = [390, 844];
-export const pixelRatio = PixelRatio.get();
 export const TRUTH_SCREEN = [Dimensions.get('window').width, Dimensions.get('window').height];
 
 export const responsiveSize = (standardSize, standardScreenSize=STANDARD_SCREEN[1], truthScreenSize=TRUTH_SCREEN[1]) => {
     return Math.round(truthScreenSize * standardSize / standardScreenSize);
 };
-
-// export const responsiveFontSize = (size) => {
-//     const factor = TRUTH_SCREEN[0] < STANDARD_SCREEN[0] ? 0.8 : STANDARD_SCREEN[0] < 780 ? 2 : 2.5;
-//     return Math.round((size * factor) / pixelRatio);
-// }
