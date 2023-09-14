5864ca207329dfd798934c9ced16b859f21a608a
tuyetanh0207 <nttanh02@gmail.com>
2023-09-14 23:13:50 +0700

    add post model

---
+++ backend/models/post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const postSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    taskName: {
+        type: String,
+        required: true,
+    },
+    taskId: {
+        type: String,
+        required: true,
+    },
+    campaignId: {
+        type: String,
+        required: true,
+
+    },
+    caption: {
+        type: String,
+        required: true,
+    },
+    photos: {
+        type: [String],
+        required: true,
+
+    },
+    address: String,
+    votedPoint: Number,
+
+})
+module.exports = mongoose.model('Post', postSchema)
