6ae8790fc5f166d19c1902d6d413749fde0662e2
ctpt41 <caothiphuongthao0401@gmail.com>
2023-09-25 10:36:03 +0700

    Revert "edit activityController"
    This reverts commit a606ab57e9853bcb70534fb2b920e5876b8810a3.

--- backend/controller/activityController.js
+++ backend/controller/activityController.js
@@ -18,11 +18,11 @@ class ActivityController {
   static show = async (req, res) => {
     try {
       // Lấy tọa độ của người dùng từ req
-      const userLatitude = parseFloat(req.query.latitude);
-      const userLongitude = parseFloat(req.query.longitude);
+      //const userLatitude = parseFloat(req.query.latitude);
+      //const userLongitude = parseFloat(req.query.longitude);
 
-      // const userLatitude = 10.781115855332459; 
-      // const userLongitude = 106.66876032407103;
+      const userLatitude = 10.781115855332459; 
+      const userLongitude = 106.66876032407103;
 
       const nearbyPlaces = [];
 
