d295bb65e5a90a7f04e4686ac759893b2bf0b847
ctpt41 <caothiphuongthao0401@gmail.com>
2023-09-29 00:10:16 +0700

    edit get tasklist

--- frontend/ExpoApp/api/activity.js
+++ frontend/ExpoApp/api/activity.js
@@ -4,15 +4,19 @@ import client from "./client"
 export const activity = async (userLatitude, userLongitude) => {
     try {
         console.log("render task list")
+
+
         const res = await client.get('/activity/tasklist', {
-            latitude: userLatitude,
-            longitude: userLongitude
+            params: {latitude: userLatitude,
+                longitude: userLongitude}
+            
         })
+
         console.log('Tasklist: ', res.data);
         return res;
 
     } catch (error) {
-        console.log('error inside render task list')
+        console.log(error)
 
     }
 }
