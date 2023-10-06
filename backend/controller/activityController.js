e2788ff362e580147f3160f5acbb713e0dd094f9
ctpt41 <94522360+ctpt41@users.noreply.github.com>
2023-10-06 17:02:00 +0700

    update api get task list

--- backend/controller/activityController.js
+++ backend/controller/activityController.js
@@ -36,6 +36,8 @@ class ActivityController {
         var task = tasklist[i];
         if(task.isContraint == false) {
           task['distance'] = 0;
+          task['latitude'] = userLatitude;
+          task['longitude'] = userLongitude;
           nearTaskList.push(task);
 
         } else {
@@ -52,37 +54,37 @@ class ActivityController {
             nearTaskList.push(task);
           }
           // update noti
-          if(distance<150 && userID != "") {
+      //     if(distance<150 && userID != "") {
 
-            var user = await User.findById(userID);
+      //       var user = await User.findById(userID);
             
-            var newNotiDate = new Date();
-            var newNoti = {
-              notiDate: newNotiDate,
-              activityID: task._id,
-              content: task.nameTask,
-              distance: distance
-            }
+      //       var newNotiDate = new Date();
+      //       var newNoti = {
+      //         notiDate: newNotiDate,
+      //         activityID: task._id,
+      //         content: task.nameTask,
+      //         distance: distance
+      //       }
               
             
-            var count = 0;
-            var sameActivityID = 0;
-            for (let i = 0; i<user.noti.length;i++) {
-              if (user.noti[i].notiDate.toString().slice(0,15) == newNotiDate.toString().slice(0,15)) {
-                count++;
-              }
-              if (user.noti[i].notiDate.toString().slice(0,15) == newNotiDate.toString().slice(0,15) && newNoti.activityID ==user.noti[i].activityID) {
-                sameActivityID = 1;
-              }
-            }
+      //       var count = 0;
+      //       var sameActivityID = 0;
+      //       for (let i = 0; i<user.noti.length;i++) {
+      //         if (user.noti[i].notiDate.toString().slice(0,15) == newNotiDate.toString().slice(0,15)) {
+      //           count++;
+      //         }
+      //         if (user.noti[i].notiDate.toString().slice(0,15) == newNotiDate.toString().slice(0,15) && newNoti.activityID ==user.noti[i].activityID) {
+      //           sameActivityID = 1;
+      //         }
+      //       }
             
-            if (count < 4 &&sameActivityID==0 ) {
+      //       if (count < 4 &&sameActivityID==0 ) {
               
-              await User.findByIdAndUpdate(userID,{$push: {noti:newNoti }})
+      //         await User.findByIdAndUpdate(userID,{$push: {noti:newNoti }})
               
-            }
-          }
-        }
+      //       }
+      //     }
+      //   }
 
       }
       
